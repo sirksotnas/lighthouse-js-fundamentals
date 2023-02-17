161b1c0ef144362ceb851c3c54712772e86e874f
Kris Santos <kris.santos101@gmail.com>
2023-02-17 14:32:56 -0600

    conditional examples

---
+++ conditionals.js
@@ -0,0 +1,34 @@
+// conditional fundamentals
+const raining = false;
+const cold = true;
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
+if (cold) {
+  console.log('Make sure you pick out a scarf!');
+}
+console.log("Now you're ready to go outside!");
+// conditional exercise: weather
+const temperature = 16;
+if (temperature < 0) {
+  console.log('Make sure you pick out a scarf!');
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+console.log("Now you're ready to go outside!");
+// using logical operator &&
+const isCitizen = true;
+const age =26;
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+// using logical operator ||
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea..");
+}
+// using logical operator !
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
